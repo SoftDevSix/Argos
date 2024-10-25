a80c845adc17b1225e7aaa2e254eb1dcd162e5b2
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-10-24 23:49:48 -0400

    ci: implemented source tree class; added path validation before the tree analisis

---
+++ src/main/java/com/softdevsix/argos/ast_analyzer/SourceTreeAnalyzer.java
@@ -0,0 +1,33 @@
+package com.softdevsix.argos.ast_analyzer;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.NoSuchFileException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SourceTreeAnalyzer {
+
+    public List<Path> getJavaFiles(Path directoryPath) throws Exception {
+        String javaExtension = ".java";
+        return getFilesByExtension(directoryPath, javaExtension);
+    }
+
+    private List<Path> getFilesByExtension(Path directoryPath, String extension) throws Exception {
+        if (!Files.exists(directoryPath)) {
+            throw new NoSuchFileException("No found directory for: " + directoryPath);
+        }
+
+        if (!Files.isDirectory(directoryPath)) {
+            throw new IllegalArgumentException("The path provided is not a directory: " + directoryPath);
+        }
+
+        try (Stream<Path> stream = Files.walk(directoryPath)) {
+            return stream
+                    .filter(file -> !Files.isDirectory(file))
+                    .filter(file -> file.toString().endsWith(extension))
+                    .collect(Collectors.toList());
+        }
+    }
+}

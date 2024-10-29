7dedec0c7fc04f8d01f2b49013016529a7a44a2c
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-10-28 23:16:13 -0400

    refactor: decoupling path validation from a directory when parsing the source tree

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/DirectoryAnalyzers/SourceTreeAnalyzer.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/DirectoryAnalyzers/SourceTreeAnalyzer.java
@@ -1,7 +1,8 @@
 package edu.usb.argos.ASTProcessor.DirectoryAnalyzers;
 
+import edu.usb.argos.ASTProcessor.Validators.PathValidators.IPathValidator;
+
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,19 +10,19 @@ import java.util.stream.Stream;
 
 public class SourceTreeAnalyzer {
 
+    private IPathValidator pathValidator;
+
+    public SourceTreeAnalyzer(IPathValidator pathValidator) {
+        this.pathValidator = pathValidator;
+    }
+
     public List<Path> getJavaFiles(Path directoryPath) throws Exception {
         String javaExtension = ".java";
         return getFilesByExtension(directoryPath, javaExtension);
     }
 
     private List<Path> getFilesByExtension(Path directoryPath, String extension) throws Exception {
-        if (!Files.exists(directoryPath)) {
-            throw new NoSuchFileException("No found directory for: " + directoryPath);
-        }
-
-        if (!Files.isDirectory(directoryPath)) {
-            throw new IllegalArgumentException("The path provided is not a directory: " + directoryPath);
-        }
+        pathValidator.validatePath(directoryPath);
 
         try (Stream<Path> stream = Files.walk(directoryPath)) {
             return stream

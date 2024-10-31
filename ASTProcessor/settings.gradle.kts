253afc581dc462e3f431221ff19411f4817198ce
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-10-31 07:25:23 -0400

    feat: add libs.versions in ASTProcessor subproject

--- ASTProcessor/settings.gradle.kts
+++ ASTProcessor/settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "ASTProcessor"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}

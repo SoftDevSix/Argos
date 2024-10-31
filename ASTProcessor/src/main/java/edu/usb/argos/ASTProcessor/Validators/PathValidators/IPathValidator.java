5661715eb6c0d76eb2967d0aec7ecebf9303d8d3
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-10-30 23:34:01 -0400

    chore: removed unneccesary files

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/Validators/PathValidators/IPathValidator.java
+++
@@ -1,9 +0,0 @@
-package edu.usb.argos.ASTProcessor.Validators.PathValidators;
-
-import java.nio.file.Path;
-
-public interface IPathValidator {
-
-    public void validatePath(Path path) throws Exception;
-
-}

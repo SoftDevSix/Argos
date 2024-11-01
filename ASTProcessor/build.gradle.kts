479b5161070b3d7d66c577b8cea31817089b427a
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-10-31 20:14:24 -0400

    feat: add .g4 for antlr grammar; add antlr library

--- ASTProcessor/build.gradle.kts
+++ ASTProcessor/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	application
 	alias(libs.plugins.springboot.web) apply true
 	alias(libs.plugins.dependency.management) apply true
+	antlr
 }
 
 group = "edu.usb.argos"
@@ -17,13 +18,30 @@ repositories {
 
 dependencies {
 	implementation(libs.springboot.starter.web)
-	implementation(libs.java.parser.core)
 	implementation(libs.springdoc.openapi)
 	developmentOnly(libs.springboot.devtools)
 	testImplementation(libs.springboot.starter.test)
 	testRuntimeOnly(libs.junit.launcher)
+	antlr(libs.antlr)
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.generateGrammarSource {
+    maxHeapSize = "64m"
+    source = fileTree("src/main/antlr") {
+        include("**/*.g4")
+    }
+    outputDirectory = file("src/main/java/edu/usb/argos/ASTProcessor/antlr")
+    arguments = listOf(
+        "-visitor",
+        "-package", "edu.usb.argos.ASTProcessor.antlr",
+        "-encoding", "UTF-8"
+    )
+}
+
+tasks.compileJava {
+	dependsOn(tasks.generateGrammarSource)
 }

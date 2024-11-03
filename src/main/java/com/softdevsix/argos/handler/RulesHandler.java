b4082e866471e66e9f5737ccfc78e25016840848
Santiago C. Saavedra <zrock_git@zohomail.com>
2024-11-03 19:34:26 -0400

    chore: integrate endpoint
    The endpoint is not using RepositoryService to validate request and
    RepositoryService is not using the RepositoryRepo to store Rules entity.
    
    - Update both classes with changes

--- src/main/java/com/softdevsix/argos/handler/RulesHandler.java
+++ src/main/java/com/softdevsix/argos/handler/RulesHandler.java
@@ -1,20 +1,28 @@
 package com.softdevsix.argos.handler;
 import com.softdevsix.argos.domain.RulesRequestMap;
+import com.softdevsix.argos.service.RulesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/argos/rules")
 public class RulesHandler {
-    private final RulesRequestMap rulesRequestMap;
+  private final RulesRequestMap rulesRequestMap;
 
-    @Autowired
-    public RulesHandler() {
-        rulesRequestMap = new RulesRequestMap() ;
-    }
-    @PostMapping
-    public ResponseEntity<Void> createRules(@RequestBody RulesRequestMap requestData) {
-        rulesRequestMap.mapFromJson(requestData);
-        return ResponseEntity.ok().build();
-    }
+  @Autowired
+  public RulesHandler() {
+    rulesRequestMap = new RulesRequestMap();
+  }
+
+  @Autowired private RulesService service;
+
+  @PostMapping
+  public ResponseEntity<Void>
+  createRules(@RequestBody RulesRequestMap requestData) {
+    rulesRequestMap.mapFromJson(requestData);
+    service.handleRules(rulesRequestMap);
+
+    return ResponseEntity.ok().build();
+  }
 }

4487ca5beffbbfe6dd91a2d4dea2dab060cb0c43
Samuel Escalera <Samuel.Escalera@fundacion-jala.org>
2024-11-05 23:50:34 -0400

    feat: unit test for lexer directory processor

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/lexer/LexerDirectoryProcessor.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/lexer/LexerDirectoryProcessor.java
@@ -1,14 +1,10 @@
 package edu.usb.argos.ASTProcessor.lexer;
 
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.ListTokenSource;
-import org.antlr.v4.runtime.Token;
 
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class LexerDirectoryProcessor {
@@ -16,23 +12,6 @@ public class LexerDirectoryProcessor {
 
     public LexerDirectoryProcessor() {
         this.fileProcessor = new LexerFileProcessor();
-    }
-
-    public CommonTokenStream getAllTokensFromAllDirectory(String directoryPath) throws IOException {
-        List<Token> allTokens = new ArrayList<>();
-
-        Files.walk(Paths.get(directoryPath))
-                .filter(Files::isRegularFile)
-                .filter(path -> path.toString().endsWith(".java"))
-                .forEach(path -> {
-                    try {
-                        allTokens.addAll(fileProcessor.getTokensFromFile(path.toString()).getTokens());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-
-        return new CommonTokenStream(new ListTokenSource(allTokens));
     }
 
     public Map<String, CommonTokenStream> getTokensFromDirectoryByFile(String directoryPath) throws IOException {

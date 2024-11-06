9da7d77c43e85642a248e770fa3dcc076f4498ef
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-11-06 19:14:08 -0400

    feat: file reader by path for antlr implemented

---
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/reader/exceptions/ParserException.java
@@ -0,0 +1,11 @@
+package edu.usb.argos.ASTProcessor.reader.exceptions;
+
+public class ParserException extends Exception {
+    public ParserException(String message) {
+        super(message);
+    }
+
+    public ParserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/reader/implementation/file/FileReaderByPath.java
@@ -0,0 +1,57 @@
+package edu.usb.argos.ASTProcessor.reader.implementation.file;
+
+import edu.usb.argos.ASTProcessor.antlr.JavaLexer;
+import edu.usb.argos.ASTProcessor.antlr.JavaParser;
+import edu.usb.argos.ASTProcessor.reader.exceptions.FileAnalyzerException;
+import edu.usb.argos.ASTProcessor.reader.exceptions.ParserException;
+import edu.usb.argos.ASTProcessor.reader.interfaces.IFileAnalyzer;
+import edu.usb.argos.ASTProcessor.reader.interfaces.IFileValidationStrategy;
+import edu.usb.argos.ASTProcessor.reader.validations.PathValidationStrategy;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileReaderByPath implements IFileAnalyzer<Path, ParseTree> {
+    private final IFileValidationStrategy<Path> validationStrategy;
+
+    public FileReaderByPath() {
+        this.validationStrategy = new PathValidationStrategy();
+    }
+
+    public FileReaderByPath(IFileValidationStrategy<Path> validationStrategy) {
+        this.validationStrategy = validationStrategy;
+    }
+
+    @Override
+    public ParseTree readFile(Path codePath) throws FileAnalyzerException {
+        try {
+            validationStrategy.validate(codePath);
+            String content = Files.readString(codePath);
+            return parseContent(content);
+        } catch (IOException e) {
+            throw new FileAnalyzerException("Error reading file: " + codePath, e);
+        } catch (ParserException e) {
+            throw new FileAnalyzerException("Error parsing file: " + codePath, e);
+        } catch (Exception e) {
+            throw new FileAnalyzerException("Error analyzing file: " + codePath, e);
+        }
+    }
+
+    private ParseTree parseContent(String content) throws ParserException {
+        try {
+            CharStream input = CharStreams.fromString(content);
+            JavaLexer lexer = new JavaLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            JavaParser parser = new JavaParser(tokens);
+
+            return parser.compilationUnit();
+        } catch (Exception e) {
+            throw new ParserException("Error to parse tree", e);
+        }
+    }
+}

ac7f33c81ac56d2b9a9c06e36f8c2ef6fefa7c3b
Samuel Escalera <Samuel.Escalera@fundacion-jala.org>
2024-11-06 07:57:26 -0400

    fix: try resource added in get tokens from directory by file

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/lexer/LexerDirectoryProcessor.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/lexer/LexerDirectoryProcessor.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class LexerDirectoryProcessor {
     private final LexerFileProcessor fileProcessor;
@@ -17,17 +18,18 @@ public class LexerDirectoryProcessor {
     public Map<String, CommonTokenStream> getTokensFromDirectoryByFile(String directoryPath) throws IOException {
         Map<String, CommonTokenStream> tokensByFile = new HashMap<>();
 
-        Files.walk(Paths.get(directoryPath))
-                .filter(Files::isRegularFile)
-                .filter(path -> path.toString().endsWith(".java"))
-                .forEach(path -> {
-                    try {
-                        CommonTokenStream tokens = fileProcessor.getTokensFromFile(path.toString());
-                        tokensByFile.put(path.toString(), tokens);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+        try (Stream<Path> paths = Files.walk(Paths.get(directoryPath))) {
+            paths.filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".java"))
+                    .forEach(path -> {
+                        try {
+                            CommonTokenStream tokens = fileProcessor.getTokensFromFile(path.toString());
+                            tokensByFile.put(path.toString(), tokens);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        }
 
         return tokensByFile;
     }

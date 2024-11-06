3c8cc84496327f455ddb7dd4cccb923181aaa3e0
Santiago C. Saavedra <zrock_git@zohomail.com>
2024-11-05 22:24:38 -0400

    feat: request project id on rules creation
    The system is creating a new project per POST request, but the endpoint
    responsibility is to only create the project rules.
    
    Update system to ask for the project ID as a parameter of the endpoint,
    this way it will validate the project ID exists and does not have
    existent defined rules.
    
    - Update handler endpoint to receive project parameter.
    - Update service to verify if the project exists and if it has rules
      already.

--- src/main/java/com/softdevsix/argos/handler/RulesHandler.java
+++ src/main/java/com/softdevsix/argos/handler/RulesHandler.java
@@ -1,9 +1,15 @@
 package com.softdevsix.argos.handler;
-import com.softdevsix.argos.domain.RulesRequestMap;
-import com.softdevsix.argos.service.RulesService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.softdevsix.argos.domain.RulesRequestMap;
+import com.softdevsix.argos.service.RulesService;
 
 @RestController
 @RequestMapping("/argos/rules")
@@ -11,20 +17,16 @@ public class RulesHandler {
   private final RulesRequestMap rulesRequestMap;
   private final RulesService service;
 
-
   @Autowired
   public RulesHandler(RulesService service) {
     this.rulesRequestMap = new RulesRequestMap();
     this.service = service;
   }
 
-  
-
   @PostMapping
-  public ResponseEntity<Void>
-  createRules(@RequestBody RulesRequestMap requestData) {
+  public ResponseEntity<Void> createRules(@RequestParam Integer project, @RequestBody RulesRequestMap requestData) {
     rulesRequestMap.mapFromJson(requestData);
-    service.handleRules(rulesRequestMap);
+    service.handleRules(rulesRequestMap, project);
 
     return ResponseEntity.ok().build();
   }

2226f5d8791649f3100f2de0c327f5ce14efc4de
Diego Centella Lopez <diego.centella.60090@usalesiana.edu.bo>
2024-11-07 09:52:07 -0400

    fix: update .g4 files and add validations for reader text

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/FileReaderByText/FileReaderByText.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/FileReaderByText/FileReaderByText.java
@@ -1,24 +1,21 @@
 package main.java.edu.usb.argos.ASTProcessor.FileReaderByText;
+
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import edu.usb.argos.ASTProcessor.antlr.JavaLexer;
 import edu.usb.argos.ASTProcessor.antlr.JavaParser;
-import java.io.IOException; 
+import java.io.IOException;
+
 public class FileReaderByText implements IFileAnalyzer<String, ParseTree> {
-    public ParseTree readFile(String content) throws Exception {
-        try {
-            return parseContent(content);
-        } catch (Exception e) {
-            throw new Exception("error to read file");
-        } 
+    public ParseTree readFile(String content) throws FileReaderException {
+        return parseContent(content);
     }
 
-    private ParseTree parseContent(String content) throws Exception {
-        if (content == null || content.trim().isEmpty()) {
-            throw new Exception("Error to parse content");
-        }
+    private ParseTree parseContent(String content) throws FileReaderException {
+        FileReaderValidation fileReaderValidation = new FileReaderValidation();
+        fileReaderValidation.validateFileReaderByText(content);
         try {
             CharStream input = CharStreams.fromString(content);
             JavaLexer lexer = new JavaLexer(input);
@@ -27,7 +24,7 @@ public class FileReaderByText implements IFileAnalyzer<String, ParseTree> {
 
             return parser.compilationUnit();
         } catch (Exception e) {
-            throw new Exception("Error to parse content");
+            throw new FileReaderException("Error to parse content");
         }
     }
 }
---
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/FileReaderByText/FileReaderException.java
@@ -0,0 +1,12 @@
+package main.java.edu.usb.argos.ASTProcessor.FileReaderByText;
+
+public class FileReaderException extends Exception {
+
+    public FileReaderException(String message) {
+        super(message);
+    }
+
+    public FileReaderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

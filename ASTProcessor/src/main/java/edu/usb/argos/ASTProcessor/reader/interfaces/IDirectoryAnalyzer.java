697d18d108ab77fbfcb62d5a0d6b6374ea270a90
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-11-07 23:59:15 -0400

    fix: brought the lastest changes by rebasing my branch

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/reader/interfaces/IDirectoryAnalyzer.java
+++
@@ -1,7 +0,0 @@
-package edu.usb.argos.ASTProcessor.reader.interfaces;
-
-import java.util.List;
-
-public interface IDirectoryAnalyzer<TSourceCode, TAst> {
-    List<TAst> analyzeDirectory(TSourceCode sourceCode);
-}

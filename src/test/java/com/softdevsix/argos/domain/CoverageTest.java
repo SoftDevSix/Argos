6cb56df3438ccd77c43ca2af22536541f787e048
Emanuel Galindo <emanuel.galindo.9362056@usalesiana.edu.bo>
2024-11-07 23:24:26 -0400

    test: Adding domain tests

---
+++ src/test/java/com/softdevsix/argos/domain/CoverageTest.java
@@ -0,0 +1,42 @@
+package com.softdevsix.argos.domain;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoverageTest {
+    @Test
+    void testCoverageReviewRequiredSetterAndGetter() {
+        Coverage coverage = new Coverage();
+        assertFalse(coverage.isCoverageReviewRequired());
+
+        coverage.setCoverageReviewRequired(true);
+        assertTrue(coverage.isCoverageReviewRequired());
+    }
+
+    @Test
+    void testMinCoveragePercentageSetterAndGetter() {
+        Coverage coverage = new Coverage();
+        assertFalse(coverage.isMinCoveragePercentageEnabled());
+
+        coverage.setMinCoveragePercentage(true);
+        assertTrue(coverage.isMinCoveragePercentageEnabled());
+    }
+
+    @Test
+    void testCoverageThresholdSetterAndGetter() {
+        Coverage coverage = new Coverage();
+        assertEquals(0, coverage.getCoverageThreshold());
+
+        coverage.setCoverageThreshold(80);
+        assertEquals(80, coverage.getCoverageThreshold());
+    }
+
+    @Test
+    void testRejectIfLowerSetterAndGetter() {
+        Coverage coverage = new Coverage();
+        assertFalse(coverage.isRejectIfLowerEnabled());
+
+        coverage.setRejectIfLower(true);
+        assertTrue(coverage.isRejectIfLowerEnabled());
+    }
+}

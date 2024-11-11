9ca03c28719584883cd25a6f7119ddabca0e0bcb
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-11-11 16:39:28 -0400

    refactor: changing to generic to more flexibility

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/interfaces/IMethodAnalyzerVisitor.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/interfaces/IMethodAnalyzerVisitor.java
@@ -2,14 +2,13 @@ package edu.usb.argos.ASTProcessor.visitor.interfaces;
 
 import edu.usb.argos.ASTProcessor.visitor.models.method.MethodInfo;
 import edu.usb.argos.ASTProcessor.visitor.models.method.ParameterInfo;
-import org.antlr.v4.runtime.ParserRuleContext;
 
 import java.util.List;
 
-public interface IMethodAnalyzerVisitor {
-    MethodInfo visitMethod(ParserRuleContext ctx);
-    int getMethodLines(ParserRuleContext ctx);
-    List<String> getMethodModifiers(ParserRuleContext ctx);
-    String getReturnType(ParserRuleContext ctx);
-    List<ParameterInfo> getParameters(ParserRuleContext ctx);
+public interface IMethodAnalyzerVisitor<T> {
+    MethodInfo visitMethod(T ctx);
+    int getMethodLines(T ctx);
+    List<String> getMethodModifiers(T ctx);
+    String getReturnType(T ctx);
+    List<ParameterInfo> getParameters(T ctx);
 }

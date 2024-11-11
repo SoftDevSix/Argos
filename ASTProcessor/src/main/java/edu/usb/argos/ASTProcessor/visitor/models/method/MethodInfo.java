d4a41854208891c9f63fac8028f5d8d41f524dd0
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-11-10 21:20:37 -0400

    feat: method model for visitor AST implemented

---
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/models/method/MethodInfo.java
@@ -0,0 +1,40 @@
+package edu.usb.argos.ASTProcessor.visitor.models.method;
+
+import java.util.List;
+
+public class MethodInfo {
+    private final String name;
+    private final String returnType;
+    private final List<String> modifiers;
+    private final List<ParameterInfo> parameters;
+    private final int lines;
+
+    public MethodInfo(String name, String returnType, List<String> modifiers,
+                      List<ParameterInfo> parameters, int lines) {
+        this.name = name;
+        this.returnType = returnType;
+        this.modifiers = modifiers;
+        this.parameters = parameters;
+        this.lines = lines;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getReturnType() {
+        return returnType;
+    }
+
+    public List<String> getModifiers() {
+        return modifiers;
+    }
+
+    public List<ParameterInfo> getParameters() {
+        return parameters;
+    }
+
+    public int getLines() {
+        return lines;
+    }
+}
---
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/models/method/ParameterInfo.java
@@ -0,0 +1,19 @@
+package edu.usb.argos.ASTProcessor.visitor.models.method;
+
+public class ParameterInfo {
+    private final String name;
+    private final String type;
+    
+    public ParameterInfo(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}

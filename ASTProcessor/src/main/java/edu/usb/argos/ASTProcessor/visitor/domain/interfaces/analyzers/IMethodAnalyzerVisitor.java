110728a421b64c606f13d68cdd6c7c894b6e9dc6
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-11-12 00:54:23 -0400

    feat: java method visitor implemented with news attributes for return information method node

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/domain/interfaces/analyzers/IMethodAnalyzerVisitor.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/domain/interfaces/analyzers/IMethodAnalyzerVisitor.java
@@ -1,13 +1,14 @@
 package edu.usb.argos.ASTProcessor.visitor.domain.interfaces.analyzers;
 
-import edu.usb.argos.ASTProcessor.visitor.domain.entities.method.MethodInfo;
-import edu.usb.argos.ASTProcessor.visitor.domain.entities.method.ParameterInfo;
+import edu.usb.argos.ASTProcessor.visitor.domain.entities.method.*;
 
 import java.util.List;
 
 public interface IMethodAnalyzerVisitor<T> {
     MethodInfo visitMethod(T ctx);
-    int getMethodLines(T ctx);
+    CodeMetrics getCodeMetrics(T ctx);
+    ComplexityMetrics getComplexityMetrics(T ctx);
+    DependencyInfo getDependencyInfo(T ctx);
     List<String> getMethodModifiers(T ctx);
     String getReturnType(T ctx);
     List<ParameterInfo> getParameters(T ctx);

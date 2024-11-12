66127c3c95c7ed9563dad6b3f7a38335e69a4bef
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-11-12 19:25:22 -0400

    refactor: remove methods from IClassAnalyzerVisitor interface

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/domain/interfaces/analyzers/classes/IClassAnalyzerVisitor.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/domain/interfaces/analyzers/classes/IClassAnalyzerVisitor.java
@@ -6,8 +6,4 @@ import edu.usb.argos.ASTProcessor.visitor.domain.entities.classes.ClassInfo;
 public interface IClassAnalyzerVisitor<T> {
 
     ClassInfo visitClassDeclaration(T ctx);
-    String getClassName(T ctx);
-    List<String> getClassModifiers(T ctx);
-    List<String> getImplementedInterfaces(T ctx);
-    String getSuperClass(T ctx);
 }

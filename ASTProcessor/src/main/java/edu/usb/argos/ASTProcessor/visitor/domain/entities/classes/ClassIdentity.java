6f26ee516e5b962d585cbd7814b48e8287fbc988
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-11-12 19:23:04 -0400

    feat: add entity class ClassIdentity

---
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/domain/entities/classes/ClassIdentity.java
@@ -0,0 +1,34 @@
+package edu.usb.argos.ASTProcessor.visitor.domain.entities.classes;
+
+import java.util.List;
+
+public class ClassIdentity {
+
+    private final String name;
+    private final String packageName;
+    private final List<String> modifiers;
+    private final List<AnnotationInfo> annotations;
+
+    public ClassIdentity(String name, String packageName, List<String> modifiers, List<AnnotationInfo> annotations) {
+        this.name = name;
+        this.packageName = packageName;
+        this.modifiers = modifiers;
+        this.annotations = annotations;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public List<String> getModifiers() {
+        return modifiers;
+    }
+
+    public List<AnnotationInfo> getAnnotations() {
+        return annotations;
+    }
+}

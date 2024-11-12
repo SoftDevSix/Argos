2052162f240de5d002410f18e92a004b3a3f77a9
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-11-12 17:16:21 -0400

    test: added unit tests to analyze class attributes from the context of a class

--- ASTProcessor/src/test/java/edu/usb/argos/ASTProcessor/AttributeAnalyzerTest/JavaAttributeVisitorTest.java
+++ ASTProcessor/src/test/java/edu/usb/argos/ASTProcessor/AttributeAnalyzerTest/JavaAttributeVisitorTest.java
@@ -1,86 +1,111 @@
-package test.java.edu.usb.argos.ASTProcessor.AttributeAnalyzerTest;
+package edu.usb.argos.ASTProcessor.AttributeAnalyzerTest;
 
+import edu.usb.argos.ASTProcessor.AttributeAnalyzer.AttributeHandler;
+import edu.usb.argos.ASTProcessor.AttributeAnalyzer.Entities.AttributeInfo;
+import edu.usb.argos.ASTProcessor.AttributeAnalyzer.JavaAttributeVisitor;
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.ParserRuleContext;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import edu.usb.argos.ASTProcessor.antlr.JavaLexer;
 import edu.usb.argos.ASTProcessor.antlr.JavaParser;
-import main.java.edu.usb.argos.ASTProcessor.AttributeAnalyzer.Entities.AttributeInfo;
-import main.java.edu.usb.argos.ASTProcessor.AttributeAnalyzer.JavaAttributeVisitor;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaAttributeVisitorTest {
 
-    private JavaAttributeVisitor visitor;
+    private static JavaAttributeVisitor visitor;
 
-    @BeforeEach
-    void setUp() {
-        visitor = new JavaAttributeVisitor();
+    @BeforeAll
+    public static void init() {
+        AttributeHandler attributeHandler = new AttributeHandler();
+        visitor = new JavaAttributeVisitor(attributeHandler);
     }
 
-    private JavaParser.FieldDeclarationContext parseFieldDeclaration(String code) {
-        JavaLexer lexer = new JavaLexer(CharStreams.fromString(code));
+    private Optional<JavaParser.ClassBodyContext> getClassFromText(String path) throws IOException {
+        CharStream input = CharStreams.fromString(path);
+
+        JavaLexer lexer = new JavaLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         JavaParser parser = new JavaParser(tokens);
-        return parser.fieldDeclaration();
+
+        JavaParser.CompilationUnitContext compilationUnitContext = parser.compilationUnit();
+
+        if (!compilationUnitContext.typeDeclaration().isEmpty()) {
+            JavaParser.TypeDeclarationContext typeDecl = compilationUnitContext.typeDeclaration().get(0);
+            if (typeDecl.classDeclaration() != null) {
+                return Optional.ofNullable(typeDecl.classDeclaration().classBody());
+            }
+        }
+
+        return Optional.empty();
     }
 
     @Test
-    void testVisitAttribute_withModifiers() {
-        String code = "public final int value;";
-        JavaParser.FieldDeclarationContext ctx = parseFieldDeclaration(code);
+    void testVisitAttribute_withModifiers() throws IOException {
+        String classBody = """
+                public class Example {
+                    private final int someValue = 12;
+                    public String name;
+                    protected double ratio = 3.14;
+                }
+                """;
+        Optional<JavaParser.ClassBodyContext> classFound = getClassFromText(classBody);
 
-        AttributeInfo attributeInfo = visitor.visitAttribute(ctx);
-
-        assertEquals("value", attributeInfo.getName());
-        assertNotNull("int", attributeInfo.getType());
-        assertNotNull(attributeInfo.getModifiers());
+        assertFalse(classFound.isEmpty());
+        for (AttributeInfo attributeInfo : visitor.visitAttribute(classFound.get())) {
+            assertFalse(visitor.getAttributeModifiers(attributeInfo).isEmpty());
+        }
     }
 
     @Test
-    void testVisitAttribute_withoutModifiers() {
-        String code = "int value;";
-        JavaParser.FieldDeclarationContext ctx = parseFieldDeclaration(code);
+    void testVisitAttribute_withoutModifiers() throws IOException {
+        String classBody = """
+                public class Example {
+                    int someValue = 12;
+                    String name;
+                    double ratio = 3.14;
+                }
+                """;
+        Optional<JavaParser.ClassBodyContext> classFound = getClassFromText(classBody);
 
-        AttributeInfo attributeInfo = visitor.visitAttribute(ctx);
-
-        assertEquals("value", attributeInfo.getName());
-        assertEquals("int", attributeInfo.getType());
-        assertTrue(attributeInfo.getModifiers().isEmpty());
+        assertFalse(classFound.isEmpty());
+        for (AttributeInfo attributeInfo : visitor.visitAttribute(classFound.get())) {
+            assertTrue(attributeInfo.getModifiers().isEmpty());
+        }
     }
 
     @Test
-    void testGetAttributeModifiers_withMultipleModifiers() {
-        String code = "private final String attribute;";
-        JavaParser.FieldDeclarationContext ctx = parseFieldDeclaration(code);
+    void testGetAttributeType() throws IOException {
+        String classBody = """
+                public class Example {
+                    private final int someValue = 12;
+                }
+                """;
+        Optional<JavaParser.ClassBodyContext> classFound = getClassFromText(classBody);
 
-        List<String> modifiers = visitor.getAttributeModifiers(ctx);
-
-        assertNotNull(modifiers);
+        assertFalse(classFound.isEmpty());
+        Optional<AttributeInfo> attributeInfo = Optional.ofNullable(visitor.visitAttribute(classFound.get()).get(0));
+        assertFalse(attributeInfo.isEmpty());
+        assertEquals("int", visitor.getAttributeType(attributeInfo.get()));
     }
 
     @Test
-    void testGetAttributeType() {
-        String code = "double number;";
-        JavaParser.FieldDeclarationContext ctx = parseFieldDeclaration(code);
+    void testGetAttributeName() throws IOException {
+        String classBody = """
+                public class Example {
+                    private final int someValue = 12;
+                }
+                """;
+        Optional<JavaParser.ClassBodyContext> classFound = getClassFromText(classBody);
 
-        String type = visitor.getAttributeType(ctx);
-
-        assertEquals("double", type);
-    }
-
-    @Test
-    void testGetAttributeName() {
-        String code = "boolean number;";
-        JavaParser.FieldDeclarationContext ctx = parseFieldDeclaration(code);
-
-        String name = visitor.getAttributeName(ctx);
-
-        assertEquals("number", name);
+        assertFalse(classFound.isEmpty());
+        Optional<AttributeInfo> attributeInfo = Optional.ofNullable(visitor.visitAttribute(classFound.get()).get(0));
+        assertFalse(attributeInfo.isEmpty());
+        assertEquals("someValue", attributeInfo.get().getName());
     }
 }

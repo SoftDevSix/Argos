1c85c336584fdf32689583d493dcb1661a466c9e
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-11-12 00:50:40 -0400

    feat: add ClassInfo entity with necessary attributes

---
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/domain/entities/classes/ClassInfo.java
@@ -0,0 +1,34 @@
+package edu.usb.argos.ASTProcessor.visitor.domain.entities.classes;
+
+import java.util.List;
+
+public class ClassInfo {
+    private final String name;
+    private final List<String> modifiers;
+    private final List<String> interfaces;
+    private final String superClass;
+
+    public ClassInfo(String name, List<String> modifiers, List<String> interfaces, String superClass) {
+        this.name = name;
+        this.modifiers = modifiers;
+        this.interfaces = interfaces;
+        this.superClass = superClass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getModifiers() {
+        return modifiers;
+    }
+
+    public List<String> getInterfaces() {
+        return interfaces;
+    }
+
+    public String getSuperClass() {
+        return superClass;
+    }
+    
+}

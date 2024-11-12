dca5a109b49242ab77f0ea59fcfea6f6e74a7b31
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-11-12 16:36:15 -0400

    refactor: deleting unnecessary classes or files

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/domain/interfaces/analyzers/IMethodAnalyzer.java
+++
@@ -1,7 +0,0 @@
-package edu.usb.argos.ASTProcessor.visitor.domain.interfaces.analyzers;
-
-import edu.usb.argos.ASTProcessor.antlr.JavaParser;
-
-public interface IMethodAnalyzer<T> {
-    T analyze(JavaParser.MethodDeclarationContext ctx);
-}

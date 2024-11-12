472870cbc7b8ae1b91d4cfe052891beb47b19eae
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-11-12 19:26:28 -0400

    feat: add IClassMemberCollector interface

---
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/domain/interfaces/analyzers/classes/IClassMemberCollector.java
@@ -0,0 +1,12 @@
+package edu.usb.argos.ASTProcessor.visitor.domain.interfaces.analyzers.classes;
+
+import edu.usb.argos.ASTProcessor.visitor.domain.entities.method.MethodInfo;
+
+import java.util.List;
+
+public interface IClassMemberCollector <T> {
+
+    List<MethodInfo> getClassMethods(T ctx);
+    // List<AttributeInfo> getClassAttributes(T ctx);
+    // List<ConstructorInfo> getClassConstructors(T ctx);
+}

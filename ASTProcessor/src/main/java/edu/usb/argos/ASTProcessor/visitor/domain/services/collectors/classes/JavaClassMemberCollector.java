664abb7b601146851c776ba20fefb9672db79db0
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-11-12 23:04:16 -0400

    feat: add logic for ClassMember

---
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/domain/services/collectors/classes/JavaClassMemberCollector.java
@@ -0,0 +1,42 @@
+package edu.usb.argos.ASTProcessor.visitor.domain.services.collectors.classes;
+
+import edu.usb.argos.ASTProcessor.antlr.JavaParser;
+import edu.usb.argos.ASTProcessor.visitor.domain.entities.method.MethodInfo;
+import edu.usb.argos.ASTProcessor.visitor.domain.interfaces.analyzers.classes.IClassMemberCollector;
+import edu.usb.argos.ASTProcessor.visitor.infraestructure.antlr.visitors.method.JavaMethodVisitor;
+import edu.usb.argos.ASTProcessor.visitor.shared.validation.ContextValidator;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import java.util.*;
+
+public class JavaClassMemberCollector implements IClassMemberCollector<ParserRuleContext> {
+    private final JavaMethodVisitor methodVisitor;
+
+    public JavaClassMemberCollector(JavaMethodVisitor methodVisitor) {
+        this.methodVisitor = methodVisitor;
+    }
+
+    @Override
+    public List<MethodInfo> getClassMethods(ParserRuleContext ctx) {
+        return ContextValidator.validateAndExecute(
+                ctx,
+                JavaParser.ClassDeclarationContext.class,
+                classCtx -> {
+                    List<MethodInfo> methods = new ArrayList<>();
+                    classCtx.classBody().classBodyDeclaration().stream()
+                            .filter(bodyDecl -> bodyDecl.memberDeclaration() != null)
+                            .filter(bodyDecl -> bodyDecl.memberDeclaration().methodDeclaration() != null)
+                            .forEach(bodyDecl -> {
+                                MethodInfo method = methodVisitor.visitMethod(
+                                        bodyDecl.memberDeclaration().methodDeclaration()
+                                );
+                                if (method != null) {
+                                    methods.add(method);
+                                }
+                            });
+                    return methods;
+                },
+                new ArrayList<>()
+        );
+    }
+}

44ebbda269e1b6aa7ee309a05646567bebe88497
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-11-12 23:05:41 -0400

    feat: add logic for ClassStructure

---
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/domain/services/collectors/classes/JavaClassStructureCollector.java
@@ -0,0 +1,44 @@
+package edu.usb.argos.ASTProcessor.visitor.domain.services.collectors.classes;
+
+import edu.usb.argos.ASTProcessor.antlr.JavaParser;
+import edu.usb.argos.ASTProcessor.visitor.domain.interfaces.analyzers.classes.IClassStructureCollector;
+import edu.usb.argos.ASTProcessor.visitor.shared.validation.ContextValidator;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import java.util.*;
+
+public class JavaClassStructureCollector implements IClassStructureCollector<ParserRuleContext> {
+    @Override
+    public String getSuperClass(ParserRuleContext ctx) {
+        return ContextValidator.validateAndExecute(
+                ctx,
+                JavaParser.ClassDeclarationContext.class,
+                classCtx -> {
+                    if (classCtx.EXTENDS() != null) {
+                        return classCtx.typeType().getText();
+                    }
+                    return null;
+                },
+                null
+        );
+    }
+
+    @Override
+    public List<String> getImplementedInterfaces(ParserRuleContext ctx) {
+        return ContextValidator.validateAndExecute(
+                ctx,
+                JavaParser.ClassDeclarationContext.class,
+                classCtx -> {
+                    List<String> interfaces = new ArrayList<>();
+                    if (classCtx.IMPLEMENTS() != null && classCtx.typeList() != null) {
+                        JavaParser.TypeListContext typeList = (JavaParser.TypeListContext) classCtx.typeList();
+                        for (JavaParser.TypeTypeContext typeType : typeList.typeType()) {
+                            interfaces.add(typeType.getText());
+                        }
+                    }
+                    return interfaces;
+                },
+                new ArrayList<>()
+        );
+    }
+}

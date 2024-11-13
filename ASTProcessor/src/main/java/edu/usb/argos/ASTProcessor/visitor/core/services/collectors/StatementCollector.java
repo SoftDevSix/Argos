63c2f616b876b72571a2652d55874839a93bf809
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-11-13 16:45:40 -0400

    chore: files structure reordered

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/core/services/collectors/StatementCollector.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/core/services/collectors/StatementCollector.java
@@ -1,8 +1,8 @@
-package edu.usb.argos.ASTProcessor.visitor.domain.services.collectors;
+package edu.usb.argos.ASTProcessor.visitor.core.services.collectors;
 
 import edu.usb.argos.ASTProcessor.antlr.JavaParser;
 import edu.usb.argos.ASTProcessor.antlr.JavaParserBaseVisitor;
-import edu.usb.argos.ASTProcessor.visitor.domain.interfaces.analyzers.IExpressionCollector;
+import edu.usb.argos.ASTProcessor.visitor.core.interfaces.collectors.IExpressionCollector;
 
 import java.util.ArrayList;
 import java.util.List;

f7fa60bd84c97a4e0ae4852ad7029cb6526a5d27
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-11-12 23:06:21 -0400

    refactor: add logic needed to return the ClassInfo

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/infraestructure/antlr/visitors/classes/JavaClassVisitor.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/infraestructure/antlr/visitors/classes/JavaClassVisitor.java
@@ -1,73 +1,61 @@
 package edu.usb.argos.ASTProcessor.visitor.infraestructure.antlr.visitors.classes;
 
-import edu.usb.argos.ASTProcessor.antlr.JavaParser;
 import edu.usb.argos.ASTProcessor.antlr.JavaParserBaseVisitor;
 import edu.usb.argos.ASTProcessor.visitor.domain.entities.classes.ClassInfo;
-import edu.usb.argos.ASTProcessor.visitor.domain.interfaces.analyzers.classes.IClassAnalyzerVisitor;
+import edu.usb.argos.ASTProcessor.visitor.domain.interfaces.analyzers.classes.*;
 import org.antlr.v4.runtime.ParserRuleContext;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
+import edu.usb.argos.ASTProcessor.visitor.domain.entities.classes.*;
 
-public class JavaClassVisitor extends JavaParserBaseVisitor<ClassInfo> implements IClassAnalyzerVisitor<ParserRuleContext> {
+public class JavaClassVisitor extends JavaParserBaseVisitor<ClassInfo>
+        implements IClassAnalyzerVisitor<ParserRuleContext> {
+
+    private final IClassIdentityCollector<ParserRuleContext> identityCollector;
+    private final IClassStructureCollector<ParserRuleContext> structureCollector;
+    private final IClassMemberCollector<ParserRuleContext> memberCollector;
+    private final IClassMetricsCollector<ParserRuleContext> metricsCollector;
+
+    public JavaClassVisitor(
+            IClassIdentityCollector<ParserRuleContext> identityCollector,
+            IClassStructureCollector<ParserRuleContext> structureCollector,
+            IClassMemberCollector<ParserRuleContext> memberCollector,
+            IClassMetricsCollector<ParserRuleContext> metricsCollector) {
+        this.identityCollector = identityCollector;
+        this.structureCollector = structureCollector;
+        this.memberCollector = memberCollector;
+        this.metricsCollector = metricsCollector;
+    }
 
     @Override
     public ClassInfo visitClassDeclaration(ParserRuleContext ctx) {
-        return validateAndExecute(ctx, classCtx -> {
-            String name = getClassName(classCtx);
-            List<String> modifiers = getClassModifiers(classCtx);
-            List<String> interfaces = getImplementedInterfaces(classCtx);
-            String superClass = getSuperClass(classCtx);
+        // Construir la identidad de la clase
+        ClassIdentity identity = new ClassIdentity(
+                identityCollector.getClassName(ctx),
+                identityCollector.getPackageName(ctx),
+                identityCollector.getClassModifiers(ctx),
+                identityCollector.getClassAnnotations(ctx)
+        );
 
-            return new ClassInfo(name, modifiers, interfaces, superClass);
-        }, null);
-    }
+        // Construir la estructura de la clase
+        ClassStructure structure = new ClassStructure(
+                structureCollector.getSuperClass(ctx),
+                structureCollector.getImplementedInterfaces(ctx)
+        );
 
-    @Override
-    public String getClassName(ParserRuleContext ctx) {
-        return validateAndExecute(ctx, classCtx -> classCtx.identifier().getText(), "");
-    }
+        // Construir los miembros de la clase
+        ClassMembers members = new ClassMembers(
+                memberCollector.getClassMethods(ctx)
+        );
 
-    @Override
-    public List<String> getClassModifiers(ParserRuleContext ctx) {
-        return validateAndExecute(ctx, classCtx -> {
-            List<String> modifiers = new ArrayList<>();
-            if (classCtx.getParent() instanceof JavaParser.ClassBodyDeclarationContext parentCtx) {
-                parentCtx.modifier().forEach(modifierContext -> modifiers.add(modifierContext.getText()));
-            }
-            return modifiers;
-        }, new ArrayList<>());
-    }
+        // Construir las métricas de la clase
+        ClassMetrics metrics = new ClassMetrics(
+                metricsCollector.getTotalLines(ctx),
+                metricsCollector.getCommentLines(ctx),
+                metricsCollector.getCommentRatio(ctx),
+                metricsCollector.getNumberOfMethods(ctx),
+                metricsCollector.getNumberOfAttributes(ctx)
+        );
 
-    @Override
-    public List<String> getImplementedInterfaces(ParserRuleContext ctx) {
-        return validateAndExecute(ctx, classCtx -> {
-            List<String> interfaces = new ArrayList<>();
-            if (classCtx.typeList() != null) {
-                classCtx.typeList().forEach(type -> interfaces.add(type.getText()));
-            }
-            return interfaces;
-        }, new ArrayList<>());
-    }
-
-    @Override
-    public String getSuperClass(ParserRuleContext ctx) {
-        return validateAndExecute(ctx, classCtx -> {
-            if (classCtx.typeType() != null) {
-                return classCtx.typeType().getText();
-            }
-            return null;
-        }, null);
-    }
-
-    private <T> T validateAndExecute(
-            ParserRuleContext ctx,
-            Function<JavaParser.ClassDeclarationContext, T> operation,
-            T defaultValue) {
-        if (ctx instanceof JavaParser.ClassDeclarationContext classCtx) {
-            return operation.apply(classCtx);
-        }
-        return defaultValue;
+        return new ClassInfo(identity, structure, members, metrics);
     }
 }

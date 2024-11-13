8b9ef14d6983d2f66f54748cd4a0f8a43a4730a9
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-11-13 19:36:46 -0400

    test: add test to evaluate the IdentityCollector class

---
+++ ASTProcessor/src/test/java/edu/usb/argos/ASTProcessor/visitor/classes/JavaClassIdentityCollectorTest.java
@@ -0,0 +1,69 @@
+package edu.usb.argos.ASTProcessor.visitor.classes;
+
+import edu.usb.argos.ASTProcessor.visitor.domain.services.collectors.classes.JavaClassIdentityCollector;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.antlr.v4.runtime.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+import edu.usb.argos.ASTProcessor.antlr.*;
+import edu.usb.argos.ASTProcessor.visitor.domain.entities.classes.*;
+
+public class JavaClassIdentityCollectorTest {
+    private JavaClassIdentityCollector classIdentityCollector;
+    private CommonTokenStream tokenStream;
+    private JavaParser parser;
+    private JavaParser.CompilationUnitContext compilationUnit;
+
+    @BeforeEach
+    void setUp() {
+        String testClass =
+                "package com.example;\n" +
+                        "\n" +
+                        "import java.util.List;\n" +
+                        "\n" +
+                        "@TestAnnotation(value = \"test\")\n" +
+                        "public class TestClass extends BaseClass implements Interface1, Interface2 {\n" +
+                        "    private String field;\n" +
+                        "    public void method() {}\n" +
+                        "}";
+
+        CharStream input = CharStreams.fromString(testClass);
+        JavaLexer lexer = new JavaLexer(input);
+        tokenStream = new CommonTokenStream(lexer);
+        parser = new JavaParser(tokenStream);
+        compilationUnit = parser.compilationUnit();
+        classIdentityCollector = new JavaClassIdentityCollector(tokenStream);
+    }
+
+    @Test
+    void getClassName_ShouldReturnCorrectClassName() {
+        JavaParser.ClassDeclarationContext ctx = compilationUnit.typeDeclaration(0).classDeclaration();
+        String className = classIdentityCollector.getClassName(ctx);
+        assertEquals("TestClass", className);
+    }
+
+    @Test
+    void getPackageName_ShouldReturnCorrectPackage() {
+        String packageName = classIdentityCollector.getPackageName(compilationUnit.packageDeclaration());
+        assertEquals("com.example", packageName);
+    }
+
+    @Test
+    void getClassModifiers_ShouldReturnCorrectModifiers() {
+        JavaParser.ClassDeclarationContext ctx = compilationUnit.typeDeclaration(0).classDeclaration();
+        List<String> modifiers = classIdentityCollector.getClassModifiers(ctx);
+        assertTrue(modifiers.contains("public"));
+    }
+
+    @Test
+    void getClassAnnotations_ShouldReturnCorrectAnnotations() {
+        JavaParser.ClassDeclarationContext ctx = compilationUnit.typeDeclaration(0).classDeclaration();
+        List<AnnotationInfo> annotations = classIdentityCollector.getClassAnnotations(ctx);
+
+        assertFalse(annotations.isEmpty());
+        assertEquals("TestAnnotation", annotations.get(0).getName());
+        assertEquals("test", annotations.get(0).getAttributes().get("value"));
+    }
+}

02af4be8b4b4a40565b544a9b86784292842f05e
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-11-15 00:22:55 -0400

    refactor: add constructors attribute in ClassMembers class

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/core/entities/classes/ClassMembers.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/core/entities/classes/ClassMembers.java
@@ -1,6 +1,6 @@
 package edu.usb.argos.ASTProcessor.visitor.core.entities.classes;
 
-import edu.usb.argos.ASTProcessor.AttributeAnalyzer.Entities.AttributeInfo;
+import edu.usb.argos.ASTProcessor.visitor.core.entities.method.AttributeInfo;
 import edu.usb.argos.ASTProcessor.visitor.core.entities.method.MethodInfo;
 
 import java.util.List;
@@ -9,17 +9,12 @@ public class ClassMembers {
 
     private final List<MethodInfo> methods;
     private final List<AttributeInfo> attributes;
-//    private final List<ConstructorInfo> constructors;
+    private final List<ConstructorInfo> constructors;
 
-//    public ClassMembers(List<MethodInfo> methods, List<AttributeInfo> attributes, List<ConstructorInfo> constructors) {
-//        this.methods = methods;
-//        this.attributes = attributes;
-//        this.constructors = constructors;
-//    }
-
-    public ClassMembers(List<MethodInfo> methods, List<AttributeInfo> attributes) {
+    public ClassMembers(List<MethodInfo> methods, List<AttributeInfo> attributes, List<ConstructorInfo> constructors) {
         this.methods = methods;
         this.attributes = attributes;
+        this.constructors = constructors;
     }
 
     public List<MethodInfo> getMethods() {
@@ -29,8 +24,8 @@ public class ClassMembers {
     public List<AttributeInfo> getAttributes() {
         return attributes;
     }
-//
-//    public List<ConstructorInfo> getConstructors() {
-//        return constructors;
-//    }
+
+    public List<ConstructorInfo> getConstructors() {
+        return constructors;
+    }
 }

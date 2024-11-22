d452ea21d8681c2421401a59a824f2ac0a4a53c3
Santiago C. Saavedra <zrock_git@zohomail.com>
2024-11-21 20:33:30 -0400

    chore: migrate ASTProcessor to code-analyzer module

--- ASTProcessor/settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "ASTProcessor"
-
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
-    }
-}

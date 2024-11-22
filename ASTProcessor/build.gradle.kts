0b62e80a1080197eb962515c4162ccebb345e2f6
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-11-22 07:48:18 -0400

    build: removed extra dependencies from lombok; dependencies sorted based on suggestions

--- ASTProcessor/build.gradle.kts
+++ ASTProcessor/build.gradle.kts
@@ -20,17 +20,15 @@ dependencies {
 	implementation(libs.springboot.starter.web)
 	implementation(libs.springdoc.openapi)
 	implementation(libs.antlr.runtime)
+	developmentOnly(libs.springboot.devtools)
 	compileOnly(libs.lombok)
 	annotationProcessor(libs.lombok)
-	developmentOnly(libs.springboot.devtools)
-	testImplementation(libs.springboot.starter.test)
-	testRuntimeOnly(libs.junit.launcher)
 	antlr(libs.antlr)
 	testImplementation(libs.logback)
-	compileOnly(libs.lombok)
-	annotationProcessor(libs.lombok)
 	testCompileOnly(libs.lombok)
 	testAnnotationProcessor(libs.lombok)
+	testImplementation(libs.springboot.starter.test)
+	testRuntimeOnly(libs.junit.launcher)
 }
 
 tasks.withType<Test> {

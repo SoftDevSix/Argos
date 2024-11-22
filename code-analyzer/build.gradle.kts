0ebff3ff4e082cd89a05932172439550fd5f75d8
Santiago C. Saavedra <zrock_git@zohomail.com>
2024-11-21 20:33:30 -0400

    chore: migrate ASTProcessor to code-analyzer module

---
+++ code-analyzer/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("buildlogic.java-library-conventions")
+    alias(libs.plugins.springboot.framework) apply true
+    alias(libs.plugins.dependency.management) apply true
+    antlr
+}
+
+dependencies {
+    implementation(libs.springboot.starter.web)
+    implementation(libs.springdoc.openapi.webmvc.ui)
+    implementation(libs.slf4j.api)
+    implementation(libs.slf4j.simple)
+    developmentOnly(libs.springboot.devtools)
+    testImplementation(libs.springboot.starter.test)
+    testRuntimeOnly(libs.junit.launcher)
+    antlr(libs.antlr)
+}
+
+val generateLexerSource by tasks.registering(AntlrTask::class) {
+    maxHeapSize = "64m"
+    source = fileTree("src/main/antlr") { include("JavaLexer.g4") }
+    arguments = listOf("-visitor", "-package", "edu.usb.argos.astprocessor.antlr", "-encoding", "UTF-8")
+    outputDirectory = file("src/main/java/edu/usb/argos/astprocessor/antlr")
+}
+
+val generateParserSource by tasks.registering(AntlrTask::class) {
+    maxHeapSize = "64m"
+    source = fileTree("src/main/antlr") { include("JavaParser.g4") }
+    arguments = listOf("-visitor", "-package", "edu.usb.argos.astprocessor.antlr", "-encoding", "UTF-8")
+    outputDirectory = file("src/main/java/edu/usb/argos/astprocessor/antlr")
+    dependsOn(generateLexerSource)
+}
+
+tasks.compileJava {
+    dependsOn(generateLexerSource, generateParserSource)
+}

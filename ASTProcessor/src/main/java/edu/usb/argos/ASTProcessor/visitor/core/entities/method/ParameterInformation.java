60216dbd0985782c2b6e61e1d2e09de1f3c2725c
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-11-22 21:36:12 -0400

    refactor: method information model improved

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/core/entities/method/ParameterInformation.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/core/entities/method/ParameterInformation.java
@@ -3,9 +3,14 @@ package edu.usb.argos.ASTProcessor.visitor.core.entities.method;
 import lombok.Builder;
 import lombok.Value;
 
+import java.util.List;
+
 @Value
 @Builder
 public class ParameterInformation {
     String name;
     String type;
+    List<String> modifiers;
+    boolean isVarArgs;
+    List<String> annotations;
 }

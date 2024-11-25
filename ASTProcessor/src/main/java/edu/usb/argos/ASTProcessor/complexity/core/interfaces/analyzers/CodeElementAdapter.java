2ed8764595c9dd9374287fdab25110006c1efbb5
axel-ayala-jalafund <axel.ayala@fundacion-jala.org>
2024-11-24 23:38:12 -0400

    refactor: models and interfaces refactored and improved

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/complexity/core/interfaces/analyzers/CodeElementAdapter.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/complexity/core/interfaces/analyzers/CodeElementAdapter.java
@@ -1,12 +1,10 @@
 package edu.usb.argos.ASTProcessor.complexity.core.interfaces.analyzers;
 
-import edu.usb.argos.ASTProcessor.visitor.core.interfaces.nodes.Expression;
 import edu.usb.argos.ASTProcessor.visitor.core.interfaces.nodes.Statement;
 
 import java.util.List;
 
-public interface CodeElementAdapter<S, E> {
+public interface CodeElementAdapter<S> {
     List<Statement<S>> getStatements();
-    List<Expression<E>> getExpressions();
     String getName();
 }

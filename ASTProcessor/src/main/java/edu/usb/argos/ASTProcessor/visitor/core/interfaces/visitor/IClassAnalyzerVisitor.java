695737fe3270f54a2c43666b9d45a82b1c8eca17
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-11-26 17:32:50 -0400

    refactor: add JavaParser.StatementContext to the required generic classes

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/core/interfaces/visitor/IClassAnalyzerVisitor.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/visitor/core/interfaces/visitor/IClassAnalyzerVisitor.java
@@ -2,7 +2,7 @@ package edu.usb.argos.ASTProcessor.visitor.core.interfaces.visitor;
 
 import edu.usb.argos.ASTProcessor.visitor.core.entities.classes.ClassInformation;
 
-public interface IClassAnalyzerVisitor<T> {
-    ClassInformation visitClass(T ctx);
+public interface IClassAnalyzerVisitor<T, S> {
+    ClassInformation<S> visitClass(T ctx);
 }
 

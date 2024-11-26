90166af14df2bb3a8e20d1d9c01f51de51d7e49f
Ignacio Villarroel <ignaciovillarroel50@gmail.com>
2024-11-26 19:44:47 -0400

    refactor: modify constructor adapter generic type

--- ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/complexity/infraestructure/adapters/ConstructorAdapter.java
+++ ASTProcessor/src/main/java/edu/usb/argos/ASTProcessor/complexity/infraestructure/adapters/ConstructorAdapter.java
@@ -3,26 +3,17 @@ package edu.usb.argos.ASTProcessor.complexity.infraestructure.adapters;
 import edu.usb.argos.ASTProcessor.antlr.JavaParser;
 import edu.usb.argos.ASTProcessor.complexity.core.interfaces.analyzers.CodeElementAdapter;
 import edu.usb.argos.ASTProcessor.visitor.core.entities.classes.ConstructorInformation;
-import edu.usb.argos.ASTProcessor.visitor.core.interfaces.nodes.Statement;
 import lombok.Value;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Value
-public class ConstructorAdapter implements CodeElementAdapter<JavaParser.StatementContext> {
-    ConstructorInformation<JavaParser.StatementContext> constructorInformation;
+public class ConstructorAdapter implements CodeElementAdapter<JavaParser.BlockStatementContext> {
+    ConstructorInformation<JavaParser.BlockStatementContext> constructorInformation;
 
     @Override
-    public List<Statement<JavaParser.StatementContext>> getStatements() {
-        return constructorInformation.getBodyStatements().stream()
-                .map(stmt -> new Statement<JavaParser.StatementContext>() {
-                    @Override
-                    public JavaParser.StatementContext getNode() {
-                        return stmt;
-                    }
-                })
-                .collect(Collectors.toList());
+    public List<JavaParser.BlockStatementContext> getStatements() {
+        return constructorInformation.getBodyStatements();
     }
 
     @Override
@@ -30,3 +21,4 @@ public class ConstructorAdapter implements CodeElementAdapter<JavaParser.Stateme
         return constructorInformation.getName();
     }
 }
+

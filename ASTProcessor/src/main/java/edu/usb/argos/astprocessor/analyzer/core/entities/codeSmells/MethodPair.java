b9c55b9acc1d03141c230a9b61701fa32eb02154
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-11-28 23:58:57 -0400

    fix: fixed rebase conflicts moving lsh candidate selector

---
+++ ASTProcessor/src/main/java/edu/usb/argos/astprocessor/analyzer/core/entities/codeSmells/MethodPair.java
@@ -0,0 +1,30 @@
+package edu.usb.argos.ASTProcessor.analyzer.core.entities.codeSmells;
+
+import lombok.ToString;
+
+import java.util.Objects;
+
+@ToString
+public class MethodPair {
+    public final String firstMethod;
+    public final String secondMethod;
+
+    public MethodPair(String firstMethod, String secondMethod) {
+        this.firstMethod = firstMethod.compareTo(secondMethod) <= 0 ? firstMethod : secondMethod;
+        this.secondMethod = firstMethod.compareTo(secondMethod) <= 0 ? secondMethod : firstMethod;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MethodPair methodPair = (MethodPair) o;
+        return firstMethod.equals(methodPair.firstMethod) &&
+                secondMethod.equals(methodPair.secondMethod);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstMethod, secondMethod);
+    }
+}
---
+++ ASTProcessor/src/main/java/edu/usb/argos/astprocessor/analyzer/infrastructure/cantidateSelectors/LSHCandidateSelector.java
@@ -0,0 +1,72 @@
+package edu.usb.argos.astprocessor.analyzer.infrastructure.cantidateSelectors;
+
+import edu.usb.argos.astprocessor.analyzer.infrastructure.config.LSHConfig;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class LSHCandidateSelector {
+
+    private final LSHConfig lshConfig;
+
+    public Set<MethodPair> findCandidatePairs(Map<String, List<Integer>> methodSignatures) {
+        Map<Integer, List<String>> bandBuckets = new HashMap<>();
+
+        for (Map.Entry<String, List<Integer>> entry : methodSignatures.entrySet()) {
+            String methodId = entry.getKey();
+            List<Integer> signature = entry.getValue();
+
+            for (int bandIndex = 0; bandIndex < lshConfig.getNumBands(); bandIndex++) {
+                List<Integer> band = signature.subList(
+                        bandIndex * lshConfig.getRowsPerBand(),
+                        (bandIndex + 1) * lshConfig.getRowsPerBand()
+                );
+
+                int bandHash = computeBandHash(band);
+                bandBuckets.computeIfAbsent(bandHash, k -> new ArrayList<>()).add(methodId);
+            }
+        }
+
+        Set<MethodPair> candidatePairs = new HashSet<>();
+        for (List<String> bucket : bandBuckets.values()) {
+            if (bucket.size() > 1) {
+                for (int i = 0; i < bucket.size(); i++) {
+                    for (int j = i + 1; j < bucket.size(); j++) {
+                        candidatePairs.add(new MethodPair(bucket.get(i), bucket.get(j)));
+                    }
+                }
+            }
+        }
+
+        return candidatePairs;
+    }
+
+    public Set<MethodPair> filterCandidates(Set<MethodPair> candidatePairs, Map<String, List<Integer>> methodSignatures, CodeMinHash codeMinHash) {
+        return candidatePairs.stream()
+                .filter(pair -> {
+                    List<Integer> signature1 = methodSignatures.get(pair.firstMethod);
+                    List<Integer> signature2 = methodSignatures.get(pair.secondMethod);
+
+                    double similarity = codeMinHash.computeSimilarity(signature1, signature2);
+                    return similarity >= lshConfig.getSimilarityThreshold();
+                })
+                .collect(Collectors.toSet());
+    }
+
+    private int computeBandHash(List<Integer> band) {
+        int hash = 0;
+
+        for (Integer value : band) {
+            hash = hash * lshConfig.getPrime() + value.hashCode();
+        }
+
+        return Math.abs(hash);
+    }
+}

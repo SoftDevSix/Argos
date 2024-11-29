f8abdb0c55e6e93fc0e60fccc79f3308f541e110
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-11-29 01:15:51 -0400

    fix: fixed rebase conflicts updating method hasher

---
+++ ASTProcessor/src/main/java/edu/usb/argos/astprocessor/analyzer/core/interfaces/IMethodStatementHasher.java
@@ -0,0 +1,7 @@
+package edu.usb.argos.astprocessor.analyzer.core.interfaces;
+
+import java.util.List;
+
+public interface IMethodStatementHasher<S> {
+    void hashMethodStatements(List<S> statements);
+}

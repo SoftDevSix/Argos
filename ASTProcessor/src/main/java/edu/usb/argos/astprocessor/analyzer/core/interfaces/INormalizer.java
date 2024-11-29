f8473efcc86e24366b2cadd9e10bf4ee5a3223fc
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-11-29 01:14:50 -0400

    fix: fixed rebase conflicts updating normalizer signature; added plain text hasher

--- ASTProcessor/src/main/java/edu/usb/argos/astprocessor/analyzer/core/interfaces/INormalizer.java
+++ ASTProcessor/src/main/java/edu/usb/argos/astprocessor/analyzer/core/interfaces/INormalizer.java
@@ -1,5 +1,5 @@
 package edu.usb.argos.astprocessor.analyzer.core.interfaces;
 
 public interface INormalizer<S> {
-    String normalizeStatement(S statement);
+    String normalize(S node);
 }
--- ASTProcessor/src/main/java/edu/usb/argos/astprocessor/analyzer/infrastructure/utils/PlainTextHasher.java
+++ ASTProcessor/src/main/java/edu/usb/argos/astprocessor/analyzer/infrastructure/utils/PlainTextHasher.java
@@ -1,4 +1,4 @@
-package edu.usb.argos.ASTProcessor.analyzer.infrastructure.utils;
+package edu.usb.argos.astprocessor.analyzer.infrastructure.utils;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;

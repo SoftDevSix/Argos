36bfada1ac12dde473187c54c5bbea5bc4dfb021
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-12-01 11:44:32 -0400

    refactor: refactores LSH class handler using new entities and modules class part of the LSH algorithm

--- ASTProcessor/src/main/java/edu/usb/argos/astprocessor/analyzer/infrastructure/utils/algorithms/lsh/LshSelector.java
+++ ASTProcessor/src/main/java/edu/usb/argos/astprocessor/analyzer/infrastructure/utils/algorithms/lsh/LshSelector.java
@@ -1,6 +1,8 @@
 package edu.usb.argos.astprocessor.analyzer.infrastructure.utils.algorithms.lsh;
 
+import edu.usb.argos.astprocessor.analyzer.core.entities.codeSmells.EntityWithSignature;
 import edu.usb.argos.astprocessor.analyzer.core.entities.codeSmells.OrderedPair;
+import edu.usb.argos.astprocessor.analyzer.core.interfaces.ISimilarityCalculator;
 import edu.usb.argos.astprocessor.analyzer.infrastructure.config.algorithms.LshConfiguration;
 import lombok.AllArgsConstructor;
 
@@ -10,52 +12,63 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
-public class LSHCandidateSelector {
+public class LshSelector<T> {
 
     private final LshConfiguration lshConfig;
+    private final ISimilarityCalculator<List<Integer>> similarityCalculator;
 
-    public Set<OrderedPair<String>> findCandidatePairs(Map<String, List<Integer>> methodSignatures) {
-        Map<Integer, List<String>> bandBuckets = new HashMap<>();
+    public Set<OrderedPair<UUID>> findCandidatePairs(List<EntityWithSignature<T, List<Integer>>> entityWithSignatures) {
+        Map<Integer, List<EntityWithSignature<T, List<Integer>>>> bandBuckets = new HashMap<>();
 
-        for (Map.Entry<String, List<Integer>> entry : methodSignatures.entrySet()) {
-            String methodId = entry.getKey();
-            List<Integer> signature = entry.getValue();
-
-            for (int bandIndex = 0; bandIndex < lshConfig.getNumberOfBands(); bandIndex++) {
-                List<Integer> band = signature.subList(
-                        bandIndex * lshConfig.getRowsPerBand(),
-                        (bandIndex + 1) * lshConfig.getRowsPerBand()
-                );
-
-                int bandHash = computeBandHash(band);
-                bandBuckets.computeIfAbsent(bandHash, k -> new ArrayList<>()).add(methodId);
-            }
+        for (EntityWithSignature<T, List<Integer>> entity : entityWithSignatures) {
+            handleEntity(entity, bandBuckets);
         }
 
-        Set<OrderedPair<String>> candidatePairs = new HashSet<>();
-        for (List<String> bucket : bandBuckets.values()) {
+        return findCandidatePairs(bandBuckets);
+    }
+
+    private void handleEntity(EntityWithSignature<T, List<Integer>> entity, Map<Integer, List<EntityWithSignature<T, List<Integer>>>> bandBuckets) {
+        for (int bandIndex = 0; bandIndex < lshConfig.getNumberOfBands(); bandIndex++) {
+            List<Integer> band = entity.getSignature().subList(
+                    bandIndex * lshConfig.getRowsPerBand(),
+                    (bandIndex + 1) * lshConfig.getRowsPerBand()
+            );
+
+            int bandHash = computeBandHash(band);
+            bandBuckets.computeIfAbsent(bandHash, k -> new ArrayList<>()).add(entity);
+        }
+    }
+
+    private Set<OrderedPair<UUID>> findCandidatePairs(Map<Integer, List<EntityWithSignature<T, List<Integer>>>> bandBuckets) {
+        Set<OrderedPair<UUID>> candidatePairs = new HashSet<>();
+        for (List<EntityWithSignature<T, List<Integer>>> bucket : bandBuckets.values()) {
             if (bucket.size() > 1) {
-                for (int i = 0; i < bucket.size(); i++) {
-                    for (int j = i + 1; j < bucket.size(); j++) {
-                        candidatePairs.add(new OrderedPair<>(bucket.get(i), bucket.get(j)));
-                    }
-                }
+                handleBudget(bucket, candidatePairs);
             }
         }
 
         return candidatePairs;
     }
 
-    public Set<OrderedPair<String>> filterCandidates(Set<OrderedPair<String>> candidatePairs, Map<String, List<Integer>> methodSignatures, CodeMinHash codeMinHash) {
+    private void handleBudget(List<EntityWithSignature<T, List<Integer>>> bucket, Set<OrderedPair<UUID>> candidatePairs) {
+        for (int i = 0; i < bucket.size(); i++) {
+            for (int j = i + 1; j < bucket.size(); j++) {
+                candidatePairs.add(new OrderedPair<>(bucket.get(i).getId(), bucket.get(j).getId()));
+            }
+        }
+    }
+
+    public Set<OrderedPair<UUID>> filterCandidates(Set<OrderedPair<UUID>> candidatePairs, Map<UUID, EntityWithSignature<T, List<Integer>>> entityMap) {
         return candidatePairs.stream()
                 .filter(pair -> {
-                    List<Integer> signature1 = methodSignatures.get(pair.firstElement());
-                    List<Integer> signature2 = methodSignatures.get(pair.secondElement());
+                    List<Integer> signature1 = entityMap.get(pair.firstElement()).getSignature();
+                    List<Integer> signature2 = entityMap.get(pair.secondElement()).getSignature();
 
-                    double similarity = codeMinHash.computeSimilarity(signature1, signature2);
+                    double similarity = similarityCalculator.computeSimilarity(signature1, signature2);
                     return similarity >= lshConfig.getSimilarityThreshold();
                 })
                 .collect(Collectors.toSet());
